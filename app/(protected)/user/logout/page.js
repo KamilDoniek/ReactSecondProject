c06ce00860d37c1aaffc8e3b772d8d6bb821c54e
KamilDoniek <kamil.doniek@microsoft.wsei.edu.pl>
2024-12-11 22:32:55 +0100

    add RegisterPage ProfilePage LogoutButton

---
+++ app/(protected)/user/logout/page.js
@@ -0,0 +1,12 @@
+function LogoutForm() {
+    return ( 
+        <>
+        <h1>
+            Logout Form
+        </h1>
+        <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Logout</button>
+        </>
+     );
+}
+
+export default LogoutForm;
---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <h1>Home</h1>
+    <button className="bg-yellow-500 p-2 rounded-lg hover:bg-yellow-700">Click</button>
+    <button className="btn btn-primary">Daisyui</button>
+    </>
+  );
+}
